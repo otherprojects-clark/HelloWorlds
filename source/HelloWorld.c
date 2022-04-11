ac989c1e817c2e47e0fb1463f38e95fb2aa3b882
Clark <daprogrammer1@gmail.com>
2022-04-11 16:55:25 +0800

    haskell

--- source/HelloWorld.c
+++ source/HelloWorld.c
@@ -11,10 +11,21 @@ gcc/clang
 
 Run it by typing this:
 
+Linux:
 gcc HelloWorld.c -o HelloWorld && ./HelloWorld
 
 or 
 
 clang HelloWorld.c -o HelloWorld && ./HelloWorld
+
+
+Windows:
+gcc HelloWorld.c -o HelloWorld.exe && HelloWorld.exe
+
+or 
+
+clang HelloWorld.c -o HelloWorld.exe && HelloWorld.exe
+
+
 
 */ 
---
+++ source/HelloWorld.hs
@@ -0,0 +1,17 @@
+main :: IO ()
+
+main = putStrLn "Hello World"
+
+{- 
+Requirements:
+ghc
+
+Run it by typing this:
+
+Linux:
+ghc HelloWorld.hs -o HelloWorld && ./HelloWorld
+
+Windows:
+ghc HelloWorld.hs -o HelloWorld.exe && HelloWorld.exe
+
+-}
