1fb4b36021f69062fcdcbb87d9ae4bdafaa1bfba
Clark <daprogrammer1@gmail.com>
2022-05-06 17:31:57 +0800

    groovy

---
+++ source/HelloWorld.groovy
@@ -0,0 +1,18 @@
+class HelloWorld {
+	public static main(args) {
+		println("Hello World")
+	}
+} 
+
+
+/*
+Requirements:
+
+Groovy
+
+Run it by typing this in your terminal::
+
+groovy HelloWorld.groovy
+
+
+*/
--- source/HelloWorld.js
+++ source/HelloWorld.js
@@ -1,1 +1,19 @@
 console.log("Hello World");
+
+/*
+Requirements:
+
+Options:
+Node
+Browser Console
+
+
+run it by typing this in your terminal:
+
+node HelloWorld.js
+
+or 
+
+copy the code and paste it to browser
+
+*/
