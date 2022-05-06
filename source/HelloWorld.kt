79fab05b2e50b5ba6511af553778c2297a363560
Clark <daprogrammer1@gmail.com>
2022-05-06 14:22:01 +0800

    add kotlin

---
+++ source/HelloWorld.kt
@@ -0,0 +1,16 @@
+fun main(){
+  println("Hello World")
+}
+
+/*
+Requirements:
+
+Options:
+- kotlinc or kotlinc-native (kotlinc-native is recommended)
+- IntelliJ IDEA
+
+Run it by typing this:
+
+kotlinc-native HelloWorld.kt -o HelloWorld && HelloWorld.exe
+
+*/
