b6c2a241f8ca4f573f95c0a8491e9d219bb0c648
Clark <daprogrammer1@gmail.com>
2022-05-06 22:45:35 +0800

    update

---
+++ source/csharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace std {
+	class Program {
+		public static void Main(String[] args){
+			Console.WriteLine("Hello World");
+		}
+	}
+}
+
+/*
+Requirements:
+
+dotnet 6 sdk
+
+Run it by typing this:
+
+dotnet run
+
+
+*/
