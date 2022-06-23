29be3c7bf131f12c2803e96aa6db54451c918cfb
Clark <daprogrammer1@gmail.com>
2022-06-23 17:35:41 +0800

    add vb

---
+++ source/HelloWorld.vb
@@ -0,0 +1,13 @@
+Imports System
+
+Module HelloWorld
+	Sub Main()
+		Console.WriteLine("Hello World")
+	End Sub
+End Module
+
+' Requirements:
+' 	Dotnet or VBC
+
+' Run it by typing this:
+' vbc HelloWorld.vb && HelloWorld
